ccd0d08a4c74707e84810e26c1bfa179c45696b2
saijayanthsri <srisaijayanth@gmail.com>
2026-06-09 12:28:51 +0530

    Refactor password generator for user-defined input
    Refactor password generation logic to use lists for characters, numbers, and symbols. Allow user-defined input for the number of characters, numbers, and symbols in the password.

--- Python/Day- 5/Password generator.py
+++ Python/Day- 5/Password generator.py
@@ -1,11 +1,33 @@
 import random
 
-char="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+char=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x', 'y', 'z']
 
-password=""
+numbers=['0', '1', '2', '3', '4',
+    '5', '6', '7', '8', '9']
 
-for i in range(8):
-    password += random.choice(char)
+symbols=['!', '@', '#', '$', '%',
+    '^', '&', '*', '(', ')']
 
-print(f"Your password",password)
+a=int(input("No of char:"))
+b=int(input("No of numbers:"))
+c=int(input("No of symbols:"))
 
+password=[]
+
+for i in range(a):
+    password.append(char[random.randint(0, 25)])
+
+for i in range(b):
+    password.append(numbers[random.randint(0, len(numbers)-1)])
+
+for i in range(c):
+    password.append(symbols[random.randint(0, 9)])
+
+random.shuffle(password)
+
+word= ''.join(password)
+
+print(f"Your password is{word}")
+                
