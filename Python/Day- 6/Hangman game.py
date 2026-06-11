0a5677e609fbad1a2cbfade2e6d773604fccd1a0
V SRI SAI JAYANTH <srisaijayanth@gmail.com>
2026-06-11 21:42:48 +0530

    Hangman game

--- Python/Day- 6/Hangman game.py
+++ Python/Day- 6/Hangman game.py
@@ -1,56 +1,100 @@
-"""
-100 Days of Code - Day 8
-Hangman Game
+import random 
+hangman_logo = r"""
+ _   _    _    _   _  ____ __  __    _    _   _
+| | | |  / \  | \ | |/ ___|  \/  |  / \  | \ | |
+| |_| | / _ \ |  \| | |  _| |\/| | / _ \ |  \| |
+|  _  |/ ___ \| |\  | |_| | |  | |/ ___ \| |\  |
+|_| |_/_/   \_\_| \_|\____|_|  |_/_/   \_\_| \_|
 """
 
-import random
-from Stages import stages
+print(hangman_logo)
+print("Welcome to HANGMAN. This edition is based on modes of trnasportation")
+print("""
+=============================
+      HANGMAN RULES
+=============================
 
-# Animal names in Portuguese
-word_list = [
-    "aardvark", "baleia", "camelo", "doninha", "elefante",
-    "flamingo", "galinha", "hiena", "iguana", "jararaca",
-    "kiwi", "lince", "morsa", "narval", "ovelha", "papagaio",
-    "quati", "rinoceronte", "suricate", "tartaruga", "urso",
-    "vagalume", "wallaby", "ximango", "yak", "zebra"
+1. The computer chooses a secret word.
+2. You guess one letter at a time.
+3. If the letter is in the word, it is revealed.
+4. If the letter is not in the word, you lose a life.
+5. You have 6 lives in total.
+6. Guess all the letters before your lives reach 0.
+
+
+Good luck!
+""")
+types_cars =[
+    "toyota",
+    "honda",
+    "ford",
+    "bmw",
+    "audi",
+    "tesla",
+    "nissan",
+    "hyundai",
+    "kia",
+    "mazda",
+    "volvo",
+    "jaguar",
+    "lexus",
+    "porsche",
+    "ferrari",
+    "lamborghini",
+    "bugatti",
+    "chevrolet",
+    "mercedes",
+    "volkswagen"
 ]
+computer_choice = random.choice(types_cars)
+print(computer_choice)
+length_word = len(computer_choice)
+for i in range (length_word):
+    print("_",end = " ")
 
-# Select a random word
-chosen_word = random.choice(word_list)
 
-# Create blank spaces for the word
-display = ["_"] * len(chosen_word)
+word = []
+for i in range (length_word):
+    word.append("_ ")
 
-lives = 6
-game_over = False
-
-print("🎮 Welcome to Hangman!")
-
-while not game_over:
-
-    print(stages[lives])
-    print(f"\nLives Remaining: {lives}")
-    print("Word:", " ".join(display))
-
-    guess = input("\nGuess a letter: ").lower()
-
-    # Check if guessed letter exists in the word
-    if guess in chosen_word:
-        for index in range(len(chosen_word)):
-            if chosen_word[index] == guess:
-                display[index] = guess
+repeat = []
+life_count = 6
+won = False 
+while life_count >0:
+    print("\n")
+    guess = input("Enter your guess ").lower()
+    if guess in computer_choice:
+        for i in range(length_word):
+            if guess == computer_choice[i]:
+                word[i] = computer_choice[i]
+        answer = "".join(word)
+        print(answer)
+        if answer == computer_choice:
+            you_win = r"""
+██╗   ██╗ ██████╗ ██╗   ██╗    ██╗    ██╗██╗███╗   ██╗
+╚██╗ ██╔╝██╔═══██╗██║   ██║    ██║    ██║██║████╗  ██║
+ ╚████╔╝ ██║   ██║██║   ██║    ██║ █╗ ██║██║██╔██╗ ██║
+  ╚██╔╝  ██║   ██║██║   ██║    ██║███╗██║██║██║╚██╗██║
+   ██║   ╚██████╔╝╚██████╔╝    ╚███╔███╔╝██║██║ ╚████║
+   ╚═╝    ╚═════╝  ╚═════╝      ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝
+"""
+            print(you_win)
+            won  =True 
+            life_count = 0
+        
+            
     else:
-        lives -= 1
-        print(f"❌ '{guess}' is not in the word.")
+        life_count -= 1
+        print(f"You guessed wrong! You have {life_count} more choices")
+        print(answer)
+if life_count == 0 and not won:
 
-    # Check win condition
-    if "_" not in display:
-        print("\n🎉 Congratulations! You guessed the word:")
-        print(chosen_word)
-        game_over = True
+    print(r"""
+  ____    _    __  __ _____    _____     _______ ____
+ / ___|  / \  |  \/  | ____|  / _ \ \   / / ____|  _ \
+| |  _  / _ \ | |\/| |  _|   | | | \ \ / /|  _| | |_) |
+| |_| |/ ___ \| |  | | |___  | |_| |\ V / | |___|  _ <
+ \____/_/   \_\_|  |_|_____|  \___/  \_/  |_____|_| \_\
+""")
 
-    # Check lose condition
-    elif lives == 0:
-        print(stages[0])
-        print(f"\n💀 Game Over! The word was '{chosen_word}'.")
-        game_over = True
+    print(f"The word was: {computer_choice}")
