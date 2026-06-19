ac134deb8a2d85fc9a23c0bb454bc206c9f1acf9
V SRI SAI JAYANTH <srisaijayanth@gmail.com>
2026-06-19 14:43:31 +0530

    Dictionary and Nesting
    On Day 10 of my Python learning journey, I explored Dictionaries and Nested Data Structures, which are powerful tools for storing and organizing data efficiently.

---
+++ Python/Day- 10/Dictionary.py
@@ -0,0 +1,20 @@
+#store data in dictionary
+
+languages={
+    "python": "Basic language for coding",
+    "java": "Intermediate language for coding",
+    "SQL": "Structured Query language"
+}
+
+print(languages["SQL"])
+
+#to add in the dict
+
+languages["html"]= "hyper"
+#print(languages)
+
+#to wipe existing dict
+empty_dictionary= {}
+
+languages= {}
+print(languages)
