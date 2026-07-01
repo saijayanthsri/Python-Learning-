e4521b705c947a116081f7e013dc07b9358223a9
V SRI SAI JAYANTH <srisaijayanth@gmail.com>
2026-07-01 10:58:24 +0530

    Nested list
    Examples of nested dictionaries and lists.

--- Python/Day- 10/Dictionary.py
+++ Python/Day- 10/Dictionary.py
@@ -18,3 +18,34 @@ empty_dictionary= {}
 
 languages= {}
 print(languages)
+
+#nested dictionary
+
+car = {
+    "maruti" : "alto",
+    "volks" : "vento",
+    "mahindra" : "scorpio",
+    "audi" : "Q7"
+}
+print(car)
+
+cars = {
+    "maruti" : ["alto", "swift", "brezza", "Xl6"],
+    "volks" : ["vento", "polo", "ameo"],
+    "mahindra" : ["scorpio", "thar", "xuv700"],
+    "audi" : ["Q7", "m30", "Q5"]
+}
+print(cars)
+
+#range in nesting
+
+print(cars["mahindra"][4])
+
+
+#nested list
+
+nested_list = ["A", "B",["C", "D"]]
+
+print(nested_list[2][1])#print "D" from the nested list
+
+
